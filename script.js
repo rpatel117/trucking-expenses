6e2e2dba037d8a081809989f78dbd4ed1654ae1e
rpatel117 <129987028+rpatel117@users.noreply.github.com>
2023-04-06 17:41:35 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -11,11 +11,35 @@ let trailer = .09;
 let commission = .1;
 let maintenance = .08
 let tire = .02
+let incomeTable = document.getElementById('incomeTable');
+let distanceTable = document.getElementById('distanceTable');
+let commissionTable = document.getElementById('commissionTable');
+let carTable = document.getElementById('carTable');
+let insuranceTable = document.getElementById('insuranceTable');
+let phoneTable = document.getElementById('phoneTable');
+let fuelTable = document.getElementById('fuelTable');
+let trailerTable = document.getElementById('trailerTable');
+let maintenanceTable = document.getElementById('maintenanceTable');
+let tireTable = document.getElementById('tireTable');
 
 
 
 function calculateExpenses(income, miles) {
     let expenses = 0;
+    incomeTable.innerHTML = income;
+    distanceTable.innerHTML = miles;
+    commissionTable.innerHTML = (income * commission);
+    carTable.innerHTML = miles * carLoan
+    insuranceTable.innerHTML = miles * insurance;
+    phoneTable.innerHTML = Math.round((miles * phone) * 100) / 100
+    fuelTable.innerHTML = miles * fuel;
+    trailerTable.innerHTML = miles * trailer;
+    maintenanceTable.innerHTML = miles * maintenance;
+    tireTable.innerHTML = miles * tire;
+
+
+
+
     income = income - (income * commission);
     expenses = miles * carLoan;
     expenses += miles * insurance;
