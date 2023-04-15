ab0fbdf24d95181d57b23ea4225d5b12eb67c8ed
rpatel117 <129987028+rpatel117@users.noreply.github.com>
2023-04-15 15:36:52 -0500

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,7 +1,5 @@
-
-
 // Import the functions you need from the SDKs you need
-//import { initializeApp } from "firebase/app";
+// import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -31,6 +29,8 @@ let trailer = .09;
 let commission = .1;
 let maintenance = .08
 let tire = .02
+let expenses = 0;
+let profit = 0;
 let incomeTable = document.getElementById('incomeTable');
 let distanceTable = document.getElementById('distanceTable');
 let commissionTable = document.getElementById('commissionTable');
@@ -41,9 +41,13 @@ let fuelTable = document.getElementById('fuelTable');
 let trailerTable = document.getElementById('trailerTable');
 let maintenanceTable = document.getElementById('maintenanceTable');
 let tireTable = document.getElementById('tireTable');
+const dateInput = document.getElementById('dateInput')
+
+
+
 
 function calculateExpenses(income, miles) {
-    let expenses = 0;
+    expenses = 0;
     incomeTable.innerHTML = Math.ceil(income);
     distanceTable.innerHTML = Math.ceil(miles);
     commissionTable.innerHTML = Math.ceil(income * commission);
@@ -63,8 +67,8 @@ function calculateExpenses(income, miles) {
     expenses += miles * trailer;
     expenses += miles * maintenance;
     expenses += miles * tire;
-
-    return income - expenses;
+    profit = Math.ceil(income - expenses);
+    return profit;
 }
 
 
@@ -77,14 +81,36 @@ if (submitBtn) {
     
     console.log(`Input 1: ${income}`);
     console.log(`Input 2: ${distance}`);
+    
     outputfield.innerHTML = calculateExpenses(parseFloat(income), parseFloat(distance))
-
+    
     const contractsRef = db.collection('contracts');
     const newContractRef = contractsRef.doc();
+
+    const inputtedDate = new Date(dateInput.value)
+    const timezoneOffset = inputtedDate.getTimezoneOffset();
+    const localTime = new Date(inputtedDate.getTime() - timezoneOffset * 60 * 1000);
+
+    console.log(dateInput.value) 
+    console.log(localTime)
+    
+
+    console.log(inputtedDate)
     // create an object to represent the data you want to write
     const newContract = {
       income: income,
-      distance: distance
+      distance: distance,
+      commission: parseInt(commissionTable.innerHTML),
+      carLoan: parseInt(carTable.innerHTML),
+      insurance: parseInt(insuranceTable.innerHTML),
+      phone: parseInt(phoneTable.innerHTML),
+      fuel: parseInt(fuelTable.innerHTML),
+      trailer: parseInt(trailerTable.innerHTML),
+      maintenance: parseInt(maintenanceTable.innerHTML),
+      tire: parseInt(tireTable.innerHTML),
+      date: inputtedDate,
+      trueDate: dateInput.value, //I'll fix this later
+      profit: profit
     };
 
     // write the data to the "users" collection
@@ -102,3 +128,59 @@ if (submitBtn) {
 }
 
 
+// Alright from here we're gonna develop retrieval
+
+const contractsRef = db.collection('contracts');
+const sd = document.getElementById('startDate');
+const ed = document.getElementById('endDate');
+const retrievalBtn = document.getElementById('retrieveData');
+const expenseReport = document.getElementById('expense-report')
+
+retrievalBtn.addEventListener('click', (event) => {
+  event.preventDefault();
+  const startDate = new Date(sd.value)
+  const endDate = new Date(ed.value)
+  console.log(startDate)
+  console.log(endDate)
+
+
+  const query = contractsRef
+                .where('date', '>=', startDate)
+                .where('date', '<=', endDate);
+
+
+
+query.get().then((querySnapshot) => {
+
+  expenseReport.innerHTML = "";
+
+  querySnapshot.forEach((doc) => {
+    // doc.data() is the data for the document
+    let data = doc.data();
+
+let html = "<table><thead><tr><th>Date</th><th>Income</th><th>Distance</th><th>Commission</th><th>Car Loan</th><th>Insurance</th><th>Phone</th><th>Trailer</th><th>Maintenance</th><th>Tire</th><th>Profits</th></tr></thead><tbody>";
+
+html += "<tr>";
+html += "<td style='padding: 7x;'>" + data.trueDate + "</td>";
+html += "<td style='padding: 7px;'>" + data.income + "</td>";
+html += "<td style='padding: 7px;'>" + data.distance + "</td>";
+html += "<td style='padding: 7px;'>" + data.commission + "</td>";
+html += "<td style='padding: 7px;'>" + data.carLoan + "</td>";
+html += "<td style='padding: 7px;'>" + data.insurance + "</td>";
+html += "<td style='padding: 7px;'>" + data.phone + "</td>";
+html += "<td style='padding: 7px;'>" + data.trailer + "</td>";
+html += "<td style='padding: 7px;'>" + data.maintenance + "</td>";
+html += "<td style='padding: 7px;'>" + data.tire + "</td>";
+html += "<td style='padding: 7px;'>" + data.profit + "</td>";
+html += "</tr>";
+
+html += "</tbody></table>";
+
+let div = document.createElement("div");
+div.innerHTML = html;
+
+    expenseReport.appendChild(div);
+    console.log(doc.id, " => ", doc.data());
+  });
+});
+});
